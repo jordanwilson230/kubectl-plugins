d8d2735700e56783eb8468f2a560b514e28d4914
Jordan Wilson <jordan@bitbrew.com>
2018-02-13 20:44:59 -0500

    Handle clusterIP assignment per namespace

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,72 +1,80 @@
 #!/bin/bash -e
 
 
-if [ -z "$KUBECTL_PLUGINS_LOCAL_FLAG_FILE" ]; then
-  echo -e "\nRequired Arg: --file\n"
-  exit 1
-fi
+## Note: This plugin does not edit any files. Updates/changes will be piped to kubectl.
 
-if [[ ! -z "$KUBECTL_PLUGINS_LOCAL_FLAG_IMAGE" ]] && [[ ! "$KUBECTL_PLUGINS_LOCAL_FLAG_IMAGE" =~ ":" ]]; then
-  IFS=$'\n'
-  available_images=($(docker search --limit=100 "nexus.bitbrew.com/$KUBECTL_PLUGINS_LOCAL_FLAG_IMAGE"))
-  for image in ${!available_images[@]}; do echo "${available_images[$image]/#/$image:  }" | sed 's|^0:|  |g' ; done | sed 's|^\([0-9]:\)|\1 |g'
-  echo -e "\nSelect the number of the image to deploy:"
-  read REPLY
-  KUBECTL_PLUGINS_LOCAL_FLAG_IMAGE=$(echo "${available_images[*]}" | grep -v STARS | head -n $REPLY | tail -n1 | cut -d ' ' -f1 | cut -d '/' -f2)
-  echo -e "\nSetting image to $KUBECTL_PLUGINS_LOCAL_FLAG_IMAGE"
-fi
+## Define some common vars
+DOCKER_REGISTRY='us.gcr.io/bb-hub-01'
 
-if [ ! -z "$KUBECTL_PLUGINS_LOCAL_FLAG_PATH" ]; then
-  KUBECTL_PLUGINS_LOCAL_FLAG_FILE="${KUBECTL_PLUGINS_LOCAL_FLAG_PATH}/${KUBECTL_PLUGINS_LOCAL_FLAG_FILE}"
-fi
-
+## Inspect user's args
+[ -z "$KUBECTL_PLUGINS_LOCAL_FLAG_FILE" ] && echo -e "\nRequired Arg: --file\n" && exit 1
+[ ! -z "$KUBECTL_PLUGINS_LOCAL_FLAG_PATH" ] && KUBECTL_PLUGINS_LOCAL_FLAG_FILE="${KUBECTL_PLUGINS_LOCAL_FLAG_PATH}/${KUBECTL_PLUGINS_LOCAL_FLAG_FILE}"
 [ ! -f "$KUBECTL_PLUGINS_LOCAL_FLAG_FILE" ] && echo -e "\nUnable to locate the specified file.\n" && exit 1
-
+[[ "$KUBECTL_PLUGINS_LOCAL_FLAG_DRY" == "true" ]] && KUBECTL_PLUGINS_LOCAL_FLAG_DRY="--dry-run" || KUBECTL_PLUGINS_LOCAL_FLAG_DRY=""
 KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE="${KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE:-$KUBECTL_PLUGINS_CURRENT_NAMESPACE}"
 
-if [[ "$KUBECTL_PLUGINS_LOCAL_FLAG_IMAGE" =~ "nexus" ]]; then
-	kubectl plugin deploy -h
-	echo -e "\nInvalid image: ${KUBECTL_PLUGINS_LOCAL_FLAG_IMAGE}\n"
-        exit 1
+
+## If user passes the --image flag without a tag (i.e., -i zookeeper), search the docker registry and present a list of options.
+if [[ ! -z "$KUBECTL_PLUGINS_LOCAL_FLAG_IMAGE" ]] && [[ ! "$KUBECTL_PLUGINS_LOCAL_FLAG_IMAGE" =~ ":" ]]; then
+    IFS=$'\n'
+    available_images=($(docker search --limit=100 "${DOCKER_REGISTRY}/$KUBECTL_PLUGINS_LOCAL_FLAG_IMAGE"))
+ # Print out versions
+    for image in ${!available_images[@]}; do
+        echo "${available_images[$image]/#/$image:  }" | sed 's|^0:|  |g'
+    done | sed 's|^\([0-9]:\)|\1 |g'
+
+ # Set the version to user's selection
+    echo -e "\nSelect the number of the image to deploy:"
+    read REPLY
+    KUBECTL_PLUGINS_LOCAL_FLAG_IMAGE=$(echo "${available_images[*]}" | grep -v STARS | head -n $REPLY | tail -n1 | cut -d ' ' -f1 | cut -d '/' -f2)
+    echo -e "\nSetting image to $KUBECTL_PLUGINS_LOCAL_FLAG_IMAGE"
 fi
 
-if [[ "$KUBECTL_PLUGINS_LOCAL_FLAG_DRY" == "true" ]]; then
-  KUBECTL_PLUGINS_LOCAL_FLAG_DRY="--dry-run"
-else
-  export KUBECTL_PLUGINS_LOCAL_FLAG_DRY=""
+
+## Set any environment/namespace specific variables that must be hardcoded
+case $KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE in
+  "staging") CLUSTER_IP='10.51.240.4' ; DASHBOARD_URL_PREFIX='master.' ;;
+  "preprod") CLUSTER_IP='10.51.240.5' ; DASHBOARD_URL_PREFIX='preprod.' ;;
+  "production") CLUSTER_IP='10.51.240.6' ; DASHBOARD_URL_PREFIX='' ;;
+esac
+
+
+if [[ "$KUBECTL_PLUGINS_LOCAL_FLAG_IMAGE" =~ "$DOCKER_REGISTRY" ]]; then
+    kubectl plugin deploy -h
+    echo -e "\nInvalid image: ${KUBECTL_PLUGINS_LOCAL_FLAG_IMAGE}\nThe Docker Registry prefix should not be included when using the -i flag. Just use the image name.\nYou can change the default registry domain in the ~/.kube/plugins/deploy/deploy.sh file if needed."
+    exit 1
 fi
+
 
 ## Determine the resource(s) to be updated.
 resource_type="$(kubectl apply -f ${KUBECTL_PLUGINS_LOCAL_FLAG_FILE} --dry-run -o=jsonpath='{.kind}')"
 
-## If the file is a configmap
+# If the file is a configmap
 if [[ "$resource_type" =~ "ConfigMap" ]] || [[ "$resource_type" =~ "NetworkPolicy" ]]; then
-   KUBECTL_PLUGINS_TEMP_FLAG_NAMESPACE="$KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE"
-   [[ "$KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE" == "production" ]] && KUBECTL_PLUGINS_TEMP_FLAG_NAMESPACE=""
-   [[ "$KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE" == "staging" ]] && KUBECTL_PLUGINS_TEMP_FLAG_NAMESPACE="master."
-
    cat "$KUBECTL_PLUGINS_LOCAL_FLAG_FILE" | sed -e "s|[Ss[Aa][Nn][Dd][Bb][Oo][Xx]|${KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE}|g; s|[Ss][Tt][Aa][Gg][Ii][Nn][Gg]|${KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE}|g; s|[Pp][Rr][Ee][Pp][Rr][Oo][Dd]|${KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE}|g; \
-       s|[Pp][Rr][Oo][Dd][Uu][Cc][Tt][Ii][Oo][Nn]|${KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE}|g; s|\(.*\)https://\(.*\).dashboard.bitbrew.com|\1${KUBECTL_PLUGINS_TEMP_FLAG_NAMESPACE}dashboard.bitbrew.com|g" | kubectl -n $KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE apply -f - $KUBECTL_PLUGINS_LOCAL_FLAG_DRY
-   echo -e "Updates applied."
+        s|[Pp][Rr][Oo][Dd][Uu][Cc][Tt][Ii][Oo][Nn]|${KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE}|g; s|\(.*\)https://\(.*\).dashboard.bitbrew.com|\1${DASHBOARD_URL_PREFIX}dashboard.bitbrew.com|g" | kubectl -n $KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE apply -f - $KUBECTL_PLUGINS_LOCAL_FLAG_DRY
+   echo -e "Update Complete."
    exit 0
+fi
 
-elif [[ "$resource_type" =~ "StatefulSet" ]] || [[ "$resource_type" =~ "Deployment" ]]; then
+# If statefulset or deployment, get the image name.
+if [[ "$resource_type" =~ "StatefulSet" ]] || [[ "$resource_type" =~ "Deployment" ]]; then
   MANIFEST_IMAGE="$(kubectl -n ${KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE} apply --dry-run -f ${KUBECTL_PLUGINS_LOCAL_FLAG_FILE} -o=jsonpath='{..image}')"
   RUNNING_IMAGE="$(kubectl -n ${KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE} get -f ${KUBECTL_PLUGINS_LOCAL_FLAG_FILE} -o=jsonpath='{..containers..image}' 2>/dev/null)" || RUNNING_IMAGE=""
 else
-  echo -e "\nAn Unknown Resource Type was Defined\n"
+  echo -e "\nAn Unknown Resource Type was Defined. Exiting!\n"
   exit 1
 fi
 
+
 ## Apply statefulset
- # If image is passed, tell kubectl to use that instead of what is written in the file.
+# Replaces anything namespace specific in the file with the targeted namespace.
+# If image is passed, tell kubectl to use that instead of what is in the user's manifest.
 if [[ ! -z "$KUBECTL_PLUGINS_LOCAL_FLAG_IMAGE" ]]; then
-   KUBECTL_PLUGINS_LOCAL_FLAG_IMAGE="nexus.bitbrew.com/${KUBECTL_PLUGINS_LOCAL_FLAG_IMAGE}"
+   KUBECTL_PLUGINS_LOCAL_FLAG_IMAGE="{DOCKER_REGISTRY}/${KUBECTL_PLUGINS_LOCAL_FLAG_IMAGE}"
    cat "$KUBECTL_PLUGINS_LOCAL_FLAG_FILE" | sed -e "s|${MANIFEST_IMAGE}|${KUBECTL_PLUGINS_LOCAL_FLAG_IMAGE}|g; s|[Ss[Aa][Nn][Dd][Bb][Oo][Xx]|${KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE}|g; s|[Ss][Tt][Aa][Gg][Ii][Nn][Gg]|${KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE}|g; s|[Pp][Rr][Ee][Pp][Rr][Oo][Dd]|${KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE}|g; s|[Pp][Rr][Oo][Dd][Uu][Cc][Tt][Ii][Oo][Nn]|${KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE}|g" | kubectl -n ${KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE} apply -f - $KUBECTL_PLUGINS_LOCAL_FLAG_DRY
    echo -e "\n\n### Summary ###\nNamespace: ${KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE}\nPrevious Image: ${RUNNING_IMAGE}\nCurrent Image:    ${KUBECTL_PLUGINS_LOCAL_FLAG_IMAGE}\n"
- # If not provided, use what is in the user supplied manifest.
 else
-   cat "$KUBECTL_PLUGINS_LOCAL_FLAG_FILE" | sed -e "s|[Ss[Aa][Nn][Dd][Bb][Oo][Xx]|${KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE}|g; s|[Ss][Tt][Aa][Gg][Ii][Nn][Gg]|${KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE}|g; s|[Pp][Rr][Ee][Pp][Rr][Oo][Dd]|${KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE}|g; s|[Pp][Rr][Oo][Dd][Uu][Cc][Tt][Ii][Oo][Nn]|${KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE}|g" | kubectl -n $KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE apply -f - $KUBECTL_PLUGINS_LOCAL_FLAG_DRY
+   cat "$KUBECTL_PLUGINS_LOCAL_FLAG_FILE" | sed -e "s|[Ss[Aa][Nn][Dd][Bb][Oo][Xx]|${KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE}|g; s|[Ss][Tt][Aa][Gg][Ii][Nn][Gg]|${KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE}|g; s|[Pp][Rr][Ee][Pp][Rr][Oo][Dd]|${KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE}|g; s|[Pp][Rr][Oo][Dd][Uu][Cc][Tt][Ii][Oo][Nn]|${KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE}|g; s|clusterIP: [0-9][0-9]\(.*\)|clusterIP: $CLUSTER_IP|g" | kubectl -n $KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE apply -f - $KUBECTL_PLUGINS_LOCAL_FLAG_DRY
    echo -e "\n\n### Success ###\nNamespace: ${KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE}\nPrevious Image: ${RUNNING_IMAGE}\nCurrent Image: ${MANIFEST_IMAGE}\n"
 fi
-
