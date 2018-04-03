aa5cf63d6b7b3cdf30c42b853a3f30c2cd45cc17
Jordan Wilson <jordan@bitbrew.com>
2018-04-03 11:46:22 -0400

    Fix image search output

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -7,58 +7,56 @@
 DOCKER_REGISTRY='us.gcr.io/myProject'
 
 ## Inspect user's args
-[ -z "$KUBECTL_PLUGINS_LOCAL_FLAG_FILE" ] && echo -e "\nRequired Arg: --file\n" && exit 1
-[ ! -z "$KUBECTL_PLUGINS_LOCAL_FLAG_PATH" ] && KUBECTL_PLUGINS_LOCAL_FLAG_FILE="${KUBECTL_PLUGINS_LOCAL_FLAG_PATH}/${KUBECTL_PLUGINS_LOCAL_FLAG_FILE}"
-[ ! -f "$KUBECTL_PLUGINS_LOCAL_FLAG_FILE" ] && echo -e "\nUnable to locate the specified file.\n" && exit 1
-[[ "$KUBECTL_PLUGINS_LOCAL_FLAG_DRY" == "true" ]] && KUBECTL_PLUGINS_LOCAL_FLAG_DRY="--dry-run" || KUBECTL_PLUGINS_LOCAL_FLAG_DRY=""
-KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE="${KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE:-$KUBECTL_PLUGINS_CURRENT_NAMESPACE}"
+FILE="${KUBECTL_PLUGINS_LOCAL_FLAG_FILE:-}"
+[[ ! "$FILE" ]] && echo -e "\nRequired Arg: --file\n" && exit 1
+[[ "$KUBECTL_PLUGINS_LOCAL_FLAG_PATH" ]] && FILE="${KUBECTL_PLUGINS_LOCAL_FLAG_PATH}/${FILE}" # Append fullpath to filename given
+[ ! -f "$FILE" ] && echo -e "\nUnable to locate the specified file.\n" && exit 1
+[[ "$KUBECTL_PLUGINS_LOCAL_FLAG_DRY" == "true" ]] && DRY="--dry-run" || DRY=""
+NAMESPACE="${KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE:-$KUBECTL_PLUGINS_CURRENT_NAMESPACE}"
+IMAGE="${KUBECTL_PLUGINS_LOCAL_FLAG_IMAGE:-}"
 
+[[ "$IMAGE" =~ "$DOCKER_REGISTRY" ]] && IMAGE=${IMAGE/$DOCKER_REGISTRY\/}
 
 ## If user passes the --image flag without a tag (i.e., -i zookeeper), search the docker registry and present a list of options.
-if [[ ! -z "$KUBECTL_PLUGINS_LOCAL_FLAG_IMAGE" ]] && [[ ! "$KUBECTL_PLUGINS_LOCAL_FLAG_IMAGE" =~ ":" ]]; then
+if [[ "$IMAGE" ]] && [[ ! "$IMAGE" =~ ":" ]]; then
     IFS=$'\n'
-    if [[ "$DOCKER_REGISTRY" =~ "gcr.io" ]]; then
-        available_images=($(gcloud container images list-tags ${DOCKER_REGISTRY}/${KUBECTL_PLUGINS_LOCAL_FLAG_IMAGE} --limit=20 --format='get(tags,timestamp.datetime)'))
-        available_images=(${available_images[*]/#/$KUBECTL_PLUGINS_LOCAL_FLAG_IMAGE:})
-    else
-        available_images=($(docker search --limit=25 "${DOCKER_REGISTRY}/$KUBECTL_PLUGINS_LOCAL_FLAG_IMAGE"))
-    fi
+    case "$DOCKER_REGISTRY" in
+        *"gcr.io"*)
+          available_images=$(gcloud container images list-tags ${DOCKER_REGISTRY}/${IMAGE} --limit=20 --format='(digest,tags,timestamp.datetime)' | grep -v TAGS) ;;
+        *)
+        available_images=($(docker search --limit=25 "${DOCKER_REGISTRY}/$IMAGE" | grep -v 'TAG\|STAR')) ;;
+    esac
 
- # List available image tags
-    echo ''
-    for image in ${!available_images[@]}; do
-        echo "${available_images[$image]/#/$image:  }" 
-    done | grep -v STAR | sed 's|^\([0-9]:\)|\1 |g'
+# Print the search results
+    count=1
+    echo;
+    for image in $available_images ; do
+        echo $image $'\t' \=\> \# [ $count ]
+        let count=count+1
+    done
 
- # Set the image to user's selection
+# Set the version to user's selection
     echo -e "\n" ; read -p "Select the number of the image to deploy: " REPLY
-    [[ "$DOCKER_REGISTRY" =~ "gcr.io" ]] && let REPLY=REPLY+1
-    KUBECTL_PLUGINS_LOCAL_FLAG_IMAGE=$(echo "${available_images[*]}" | grep -v STARS | head -n $REPLY | tail -n1 | cut -d $'\t' -f1 | cut -d ' ' -f1 | cut -d '/' -f2 | sed 's|latest\;||g')
-    echo -e "\nSetting image to $KUBECTL_PLUGINS_LOCAL_FLAG_IMAGE"
+    TAG=$(echo "$available_images" | grep -v TAGS | head -n $REPLY | tail -n1 | cut -d ',' -f1 | sed 's|[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]\(.*\)||g' | cut -d ' ' -f2-3 | tr -d ' ')
+    IMAGE="${DOCKER_REGISTRY}/${IMAGE}:${TAG}"
+    echo -e "\nSetting image to $IMAGE"
 fi
-
-if [[ "$KUBECTL_PLUGINS_LOCAL_FLAG_IMAGE" =~ "$DOCKER_REGISTRY" ]]; then
-    kubectl plugin deploy -h
-    echo -e "\nInvalid image: ${KUBECTL_PLUGINS_LOCAL_FLAG_IMAGE}\nThe Docker Registry prefix should not be included when using the -i flag. Just use the image name.\nYou can change the default registry domain in the ~/.kube/plugins/deploy/deploy.sh file if needed."
-    exit 1
-fi
-
 
 ## Determine the resource(s) to be updated.
-resource_type="$(kubectl apply -f ${KUBECTL_PLUGINS_LOCAL_FLAG_FILE} --dry-run -o=jsonpath='{.kind}')"
+resource_type="$(kubectl apply -f ${FILE} --dry-run -o=jsonpath='{.kind}')"
 
 # If the file is a configmap, cronjob, or rbac
 if [[ "$resource_type" =~ "ConfigMap" ]] || [[ "$resource_type" =~ "ClusterRoleBinding" ]] || [[ "$resource_type" =~ "ClusterRole" ]] || [[ "$resource_type" =~ "RoleBinding" ]] || [[ "$resource_type" =~ "ServiceAccount" ]] || [[ "$resource_type" =~ "NetworkPolicy" ]] || [[ "$resource_type" =~ "CronJob" ]]; then
-   cat "$KUBECTL_PLUGINS_LOCAL_FLAG_FILE" | sed -e "s|[Ss[Aa][Nn][Dd][Bb][Oo][Xx]|${KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE}|g; s|[Ss][Tt][Aa][Gg][Ii][Nn][Gg]|${KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE}|g; s|[Pp][Rr][Ee][Pp][Rr][Oo][Dd]|${KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE}|g; \
-        s|[Pp][Rr][Oo][Dd][Uu][Cc][Tt][Ii][Oo][Nn]|${KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE}|g" | kubectl -n $KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE apply -f - $KUBECTL_PLUGINS_LOCAL_FLAG_DRY
+   cat "$FILE" | sed -e "s|[Ss[Aa][Nn][Dd][Bb][Oo][Xx]|${NAMESPACE}|g; s|[Ss][Tt][Aa][Gg][Ii][Nn][Gg]|${NAMESPACE}|g; s|[Pp][Rr][Ee][Pp][Rr][Oo][Dd]|${NAMESPACE}|g; \
+        s|[Pp][Rr][Oo][Dd][Uu][Cc][Tt][Ii][Oo][Nn]|${NAMESPACE}|g" | kubectl -n $NAMESPACE apply -f - $DRY
    echo -e "Update Complete."
    exit 0
 fi
 
 # If statefulset or deployment, get the image name.
 if [[ "$resource_type" =~ "StatefulSet" ]] || [[ "$resource_type" =~ "Deployment" ]]; then
-  MANIFEST_IMAGE="$(kubectl -n ${KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE} apply --dry-run -n null -f ${KUBECTL_PLUGINS_LOCAL_FLAG_FILE} -o=jsonpath='{..image}')"
-  RUNNING_IMAGE="$(kubectl -n ${KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE} get -f ${KUBECTL_PLUGINS_LOCAL_FLAG_FILE} -o=jsonpath='{..containers..image}' 2>/dev/null)" || RUNNING_IMAGE=""
+  MANIFEST_IMAGE="$(kubectl -n ${NAMESPACE} apply --dry-run -n null -f ${FILE} -o=jsonpath='{..image}')"
+  RUNNING_IMAGE="$(kubectl -n ${NAMESPACE} get -f ${FILE} -o=jsonpath='{..containers..image}' 2>/dev/null)" || RUNNING_IMAGE=""
 else
   echo -e "\nAn Unknown Resource Type was Defined. Exiting!\n"
   exit 1
@@ -68,11 +66,11 @@ fi
 ## Apply statefulset
 # Replaces anything namespace specific in the file with the targeted namespace.
 # If image is passed, tell kubectl to use that instead of what is in the user's manifest.
-if [[ ! -z "$KUBECTL_PLUGINS_LOCAL_FLAG_IMAGE" ]]; then
-   KUBECTL_PLUGINS_LOCAL_FLAG_IMAGE="${DOCKER_REGISTRY}/${KUBECTL_PLUGINS_LOCAL_FLAG_IMAGE}"
-   cat "$KUBECTL_PLUGINS_LOCAL_FLAG_FILE" | sed -e "s|${MANIFEST_IMAGE}|${KUBECTL_PLUGINS_LOCAL_FLAG_IMAGE}|g; s|[Ss[Aa][Nn][Dd][Bb][Oo][Xx]|${KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE}|g; s|[Ss][Tt][Aa][Gg][Ii][Nn][Gg]|${KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE}|g; s|[Pp][Rr][Ee][Pp][Rr][Oo][Dd]|${KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE}|g; s|[Pp][Rr][Oo][Dd][Uu][Cc][Tt][Ii][Oo][Nn]|${KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE}|g" | kubectl -n ${KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE} apply -f - $KUBECTL_PLUGINS_LOCAL_FLAG_DRY
-   echo -e "\n\n### Summary ###\nNamespace: ${KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE}\nPrevious Image: ${RUNNING_IMAGE}\nCurrent  Image:    ${KUBECTL_PLUGINS_LOCAL_FLAG_IMAGE}\n"
+if [[ ! -z "$IMAGE" ]]; then
+   IMAGE="${IMAGE}"
+   cat "$FILE" | sed -e "s|${MANIFEST_IMAGE}|${IMAGE}|g; s|[Ss[Aa][Nn][Dd][Bb][Oo][Xx]|${NAMESPACE}|g; s|[Ss][Tt][Aa][Gg][Ii][Nn][Gg]|${NAMESPACE}|g; s|[Pp][Rr][Ee][Pp][Rr][Oo][Dd]|${NAMESPACE}|g; s|[Pp][Rr][Oo][Dd][Uu][Cc][Tt][Ii][Oo][Nn]|${NAMESPACE}|g" | kubectl -n ${NAMESPACE} apply -f - $DRY
+   echo -e "\n\n### Summary ###\nNamespace: ${NAMESPACE}\nPrevious Image: ${RUNNING_IMAGE}\nCurrent  Image: ${IMAGE}\n"
 else
-   cat "$KUBECTL_PLUGINS_LOCAL_FLAG_FILE" | sed -e "s|[Ss[Aa][Nn][Dd][Bb][Oo][Xx]|${KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE}|g; s|[Ss][Tt][Aa][Gg][Ii][Nn][Gg]|${KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE}|g; s|[Pp][Rr][Ee][Pp][Rr][Oo][Dd]|${KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE}|g; s|[Pp][Rr][Oo][Dd][Uu][Cc][Tt][Ii][Oo][Nn]|${KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE}|g; s|clusterIP: [0-9][0-9]\(.*\)|clusterIP: $CLUSTER_IP|g" | kubectl -n $KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE apply -f - $KUBECTL_PLUGINS_LOCAL_FLAG_DRY
-   echo -e "\n\n### Success ###\nNamespace: ${KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE}\nPrevious Image: ${RUNNING_IMAGE}\nCurrent  Image: ${MANIFEST_IMAGE}\n"
+   cat "$FILE" | sed -e "s|[Ss[Aa][Nn][Dd][Bb][Oo][Xx]|${NAMESPACE}|g; s|[Ss][Tt][Aa][Gg][Ii][Nn][Gg]|${NAMESPACE}|g; s|[Pp][Rr][Ee][Pp][Rr][Oo][Dd]|${NAMESPACE}|g; s|[Pp][Rr][Oo][Dd][Uu][Cc][Tt][Ii][Oo][Nn]|${NAMESPACE}|g; s|clusterIP: [0-9][0-9]\(.*\)|clusterIP: $CLUSTER_IP|g" | kubectl -n $NAMESPACE apply -f - $DRY
+   echo -e "\n\n### Success ###\nNamespace: ${NAMESPACE}\nPrevious Image: ${RUNNING_IMAGE}\nCurrent  Image: ${MANIFEST_IMAGE}\n"
 fi
