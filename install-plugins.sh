08e0a48edc19885f9d82515da8d7cc719d9372a6
jordan <jordan@bitbrew.com>
2018-11-30 17:53:48 -0500

    Use ex instead of sed

--- install-plugins.sh
+++ install-plugins.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-
+# Move plugins and add location to path (required for kubectl 1.12+)
 chmod +x ./kubectl-*
 rm -rf ~/.kube/plugins/jordanwilson230
 mkdir -p ~/.kube/plugins/jordanwilson230
@@ -10,6 +10,8 @@ grep 'PATH=$PATH:~/.kube/plugins/jordanwilson230' ~/.bash_profile 1>/dev/null
 [ $? -ne 0 ] && echo 'export PATH=$PATH:~/.kube/plugins/jordanwilson230' >> ~/.bash_profile
 
 # Removes old installer function if exists
-sed -i '' 's|function kubectl\(.*\)||g' ~/.bash_profile
+ex '+g/function kubectl()/d' -cwq ~/.bash_profile
 
+# Finished
 echo -e "\nDone.\nPlease open a new terminal or run: source ~/.bash_profile\n"
+kubectl plugin list --name-only | sed 's|-| |g'
