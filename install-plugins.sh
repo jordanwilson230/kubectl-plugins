c28c9042edaaef239e5b42d9e8f857063edbf6e1
Jefferson Faseler <jjfaseler@gmail.com>
2020-10-08 21:05:15 -0500

    Dynamically install plugin

--- install-plugins.sh
+++ install-plugins.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
+case $SHELL in
+  *zsh* )
+    profile=.zshrc
+    ;;
+  * )
+    profile=.bash_profile
+    ;;
+esac
+
+echo "Detected shell: $SHELL"
+echo "Installing kubectl-plugins to ~/$profile"
 
 # Move plugins and add location to path (required for kubectl 1.12+)
 chmod +x ./kubectl-*
 rm -rf ~/.kube/plugins/jordanwilson230
 mkdir -p ~/.kube/plugins/jordanwilson230
 cp -r ./kubectl-* ~/.kube/plugins/jordanwilson230/
-grep 'PATH=$PATH:~/.kube/plugins/jordanwilson230' ~/.bash_profile 1>/dev/null
-[ $? -ne 0 ] && echo 'export PATH=$PATH:~/.kube/plugins/jordanwilson230' >> ~/.bash_profile
+grep 'PATH=$PATH:~/.kube/plugins/jordanwilson230' ~/$profile 1>/dev/null
+[ $? -ne 0 ] && echo 'export PATH=$PATH:~/.kube/plugins/jordanwilson230' >> ~/$profile
 
 # Removes old installer function if exists
-ex '+g/function kubectl()/d' -cwq ~/.bash_profile >/dev/null 2>&1
+ex '+g/function kubectl()/d' -cwq ~/$profile >/dev/null 2>&1
 
 # Finished
-echo -e "\nDone.\nPlease open a new terminal or run: source ~/.bash_profile\n"
+echo -e "\nDone.\nPlease open a new terminal or run: source ~/$profile\n"
 kubectl plugin list --name-only | sed 's|-| |g'
